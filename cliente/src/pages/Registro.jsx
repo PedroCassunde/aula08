2e77380b582c36a74e1f2f09242f9f98248d3c15
PedroCassunde <pedro.lima4@estudante.ifms.edu.br>
2024-11-26 13:13:02 -0400

    whenwilliseeyouagain

--- cliente/src/pages/Registro.jsx
+++ cliente/src/pages/Registro.jsx
@@ -2,8 +2,43 @@ import { useState } from "react";
 
 export default function Registrar() {
 
+  const [Nome, setNome] = useState([]);
+  const [Email, setEmail] = useState([]);
+
+  const Registrar = async (event) => {
+event.preventDefault();
+try {
+  await fetch('http://localhost:3000/usuarios', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      nome: nome,
+      email: email
+    })
+  });
+} catch {
+  alert("Ocorreu um erro na aplicação");
+}}
 
   return (
-        
+        <>
+        <main>
+          <form onSubmit={Registrar}>
+            <input
+            type="text"
+            value={Nome}
+            placeholder="Nome"
+            onChange={(event) => setNome(event.target,value)}/>
+
+            <input
+            type="text"
+            value={Email}
+            placeholder="Email"
+            onChange={(event) => setEmail(event.target,value)}/>
+            <button
+            type="submit">Registrar</button>
+          </form>
+        </main>
+        </>
   );
 }
